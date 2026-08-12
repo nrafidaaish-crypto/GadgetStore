c97a19e73941762787146e1beffd35e0aaadf7ea
nrafidaaish-crypto <nrafidaaish@gmail.com>
2026-08-12 10:13:46 +0700

    Update data.js

--- data.js
+++ data.js
@@ -18,7 +18,7 @@ let products = [
       "BIRU": "ip16.jpg"
     },
     img: "ip16.jpg",
-    video: "https://assets.mixkit.co/videos/preview/mixkit-hands-holding-a-smartphone-with-a-green-screen-41532-large.mp4",
+    video: "ip16.mp4",
     desc: "iPhone 16 hadir dengan layar Super Retina XDR OLED 6,1 inci, ditenagai chip A18 (3 nm) yang kencang, RAM 8 GB, serta kamera utama Fusion 48 MP dengan susunan vertikal. Ponsel ini dilengkapi tombol baru Camera Control dan Action Button, serta pilihan warna: Hitam, Putih, Pink, Hijau Kebiruan, dan Ultramarine",
     reviews: [
       { name: "Seraphine Azellie", rating: 5, date: "02 Ags 2026", variant: "Warna: TITANIUM", comment: "Layar OLED-nya super tajam, kamera malamnya jernih banget tanpa noise! ❤️❤️" },
@@ -38,7 +38,7 @@ let products = [
       "SILVER": " ip13.jpg"
     },
     img: "ip13.jpg",
-    video: "https://assets.mixkit.co/videos/preview/mixkit-hands-typing-on-a-laptop-41528-large.mp4",
+    video: "ip13.mp4",
     desc: "Apple iPhone 13 (dirilis 2021) dibekali layar Super Retina XDR OLED 6,1 inci, chipset Apple A15 Bionic (5 nm), RAM 4 GB, penyimpanan 128GB/256GB/512GB, kamera ganda belakang 12 MP (utama dan ultra-wide), kamera depan 12 MP, serta baterai 3.240 mAh dengan port Lightning.",
     reviews: [
       { name: "Vathea Anasya", rating: 5, date: "03 Ags 2026", variant: "Warna: HITAM", comment: "Game AAA rata kanan dapet 120+ FPS! System pendinginnya sangat adem." }
@@ -58,7 +58,7 @@ let products = [
       "PINK": "ip17air.jpg"
     },
     img: "ip17air.jpg",
-    video: "https://assets.mixkit.co/videos/preview/mixkit-close-up-of-a-person-using-a-smartwatch-41530-large.mp4",
+    video: "ip17air.mp4",
     desc: "iPhone Air (sering disebut iPhone 17 Air) hadir sebagai lini iPhone tertipis dari Apple dengan ketebalan hanya sekitar 5,6 mm dan bobot ringan 145 gram. Ponsel ini mengusung layar Super Retina XDR OLED 6,5 inci (120 Hz ProMotion, 3000 nits), ditenagai chip Apple A19 Pro, RAM 12 GB, kamera utama tunggal Fusion 48 MP, kamera depan 18 MP, serta dukungan khusus eSIM",
     reviews: [
       { name: "Yesava Maureen", rating: 5, date: "04 Ags 2026", variant: "Warna: SILVR", comment: "Sensor detak jantung dan pelacak tidurnya sangat akurat. Tampilan layarnya jernih!" }
@@ -78,7 +78,7 @@ let products = [
       "PINK": "ip18pro.jpg"
     },
     img: "ip18pro.jpg",
-    video: "https://assets.mixkit.co/videos/preview/mixkit-man-putting-on-wireless-headphones-41531-large.mp4",
+    video: "ip18pro.mp4",
     desc: "iPhone 18 Pro adalah calon ponsel pintar flagship dari Apple yang dirumorkan rilis pada September 2026. Perangkat ini kabarnya dibekali layar 6,3 inci dengan Dynamic Island lebih kecil, ditenagai cip Apple A20 Pro berfabrikasi 2nm, RAM 12GB, serta inovasi kamera dengan fitur variable aperture.",
     reviews: [
       { name: "Clarissa Devi", rating: 5, date: "01 Ags 2026", variant: "Warna: PUTIH", comment: "ANC-nya kedap banget, suara bass mantap dan treble sangat bening!" }
@@ -97,7 +97,7 @@ let products = [
       "SILVER": "ip16promax.jpg"
     },
     img: "ip16promax.jpg",
-    video: "https://assets.mixkit.co/videos/preview/mixkit-hands-typing-on-a-laptop-41528-large.mp4",
+    video: "ip16promax.mp4",
     desc: "iPhone 16 Pro Max adalah ponsel flagship terbesar dari Apple yang dirilis pada September 2024. Ponsel ini dibekali layar Super Retina XDR OLED 6,9 inci, chip Apple A18 Pro yang mendukung kecerdasan buatan, sistem kamera pro 48 MP dengan tombol Camera Control baru, serta baterai berkapasitas 4.685 mAh yang tahan lama",
     reviews: [
       { name: "Audrey Tampi", rating: 5, date: "29 Jul 2026", variant: "Warna: GRAY", comment: "Sangat cocok untuk ilustrasi digital dan editing video on-the-go." }
@@ -116,7 +116,7 @@ let products = [
       "SILVER": "ip14pro.jpg"
     },
     img: "ip14pro.jpg",
-    video: "https://assets.mixkit.co/videos/preview/mixkit-hands-holding-a-smartphone-with-a-green-screen-41532-large.mp4",
+    video: "ip14pro.mp4",
     desc: "iPhone 14 Pro adalah ponsel pintar kelas atas dari Apple yang dirilis pada September 2022. Ponsel ini hadir dengan layar Super Retina XDR 6,1 inci, fitur interaktif Dynamic Island, kamera utama 48 MP, serta ditenagai oleh cip cepat Apple A16 Bionic.",
     reviews: [
       { name: "Nadia Vanessa", rating: 5, date: "25 Jul 2026", variant: "Warna: HITAM", comment: "Autofokus kilat dan Kualitas warna videonya sangat natural!" }
@@ -135,7 +135,7 @@ let products = [
       "PUTIH": "ip17.jpg"
     },
     img: "ip17.jpg",
-    video: "https://assets.mixkit.co/videos/preview/mixkit-hands-typing-on-a-laptop-41528-large.mp4",
+    video: "ip17.mp4",
     desc: "iPhone 17 hadir dengan layar Super Retina XDR OLED 6,3 inci berteknologi ProMotion 120Hz, ditenagai chip Apple A19, serta dibekali kamera utama Fusion 48 MP dan kamera depan berfitur Center Stage. Ponsel ini menawarkan performa lebih tangguh dan efisiensi daya yang optimal.",
     reviews: [
       { name: "Gisca Amelia", rating: 5, date: "05 Ags 2026", variant: "Warna: HITAM", comment: "Sensasi main game PC berat di mana aja lancar jaya!" }
@@ -154,7 +154,7 @@ let products = [
       "PUTIH": "ip16pro.jpg"
     },
     img: "ip16pro.jpg",
-    video: "https://assets.mixkit.co/videos/preview/mixkit-hands-typing-on-a-laptop-41528-large.mp4",
+    video: "ip16pro.mp4",
     desc: "iPhone 16 Pro memiliki layar Super Retina XDR 6,3 inci, ditenagai chip Apple A18 Pro dengan RAM 8GB, serta dibekali kamera utama 48MP, ultrawide 48MP, dan telefoto 5x zoom. Ponsel ini menggunakan bodi titanium dengan berat 199 gram dan mendukung fitur tombol Kamera Control.",
     reviews: [
       { name: "Lianna Felicia", rating: 5, date: "04 Ags 2026", variant: "Warna: HITAM", comment: "Typing feel empuk dan suara ketukannya thocky banget!" }
@@ -173,7 +173,7 @@ let products = [
       "SILVER": "ip17pro.jpg"
     },
     img: "ip17pro.jpg",
-    video: "https://assets.mixkit.co/videos/preview/mixkit-man-putting-on-wireless-headphones-41531-large.mp4",
+    video: "ip17pro.mp4",
     desc: "iPhone 17 Pro adalah ponsel pintar andal dari Apple yang rilis pada September 2025. Ponsel ini mengusung layar OLED 6,3 inci, bodi unibody aluminium, chip A19 Pro, RAM 12 GB, sistem tiga kamera belakang 48 MP, serta daya tahan baterai yang lebih kuat.",
     reviews: [
       { name: "Shafira Aris", rating: 5, date: "06 Ags 2026", variant: "Warna: HITAM", comment: "Bantalan telinga empuk tidak bikin sakit meski dipakai bekerja seharian." }
